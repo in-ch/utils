4938b9c2444dc2eb9689dd3aa72c9e367d7e1257
in-ch <sxin2949@naver.com>
2024-11-13 16:56:10 +0900

    project init

---
+++ packages/isEmpty/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @param {unknown} value 
+ * @returns {boolean} whether the value is empty
+ */
+const isEmpty = (value: unknown): boolean => {
+    return (
+        value === null ||
+        value === undefined ||
+        (typeof value === 'object' &&
+            ((Array.isArray(value) && value.length === 0) ||
+            (value instanceof Set && value.size === 0) ||
+            (value instanceof Map && value.size === 0) ||
+            (Object.keys(value).length === 0)))
+    );
+};
