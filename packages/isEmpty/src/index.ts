4d53a76c6c1ef98b78b09a83b292a644801a715e
in-ch <sxin2949@naver.com>
2024-11-15 00:46:43 +0900

    chore: lint check

--- packages/isEmpty/src/index.ts
+++ packages/isEmpty/src/index.ts
@@ -1,15 +1,15 @@
 /**
- * @param {unknown} value 
+ * @param {unknown} value
  * @returns {boolean} whether the value is empty
  */
 export const isEmpty = (value: unknown): boolean => {
-    return (
-        value === null ||
-        value === undefined ||
-        (typeof value === 'object' &&
-            ((Array.isArray(value) && value.length === 0) ||
-            (value instanceof Set && value.size === 0) ||
-            (value instanceof Map && value.size === 0) ||
-            (Object.keys(value).length === 0)))
-    );
+  return (
+    value === null ||
+    value === undefined ||
+    (typeof value === "object" &&
+      ((Array.isArray(value) && value.length === 0) ||
+        (value instanceof Set && value.size === 0) ||
+        (value instanceof Map && value.size === 0) ||
+        Object.keys(value).length === 0))
+  );
 };
